806ef38172178537ff0baa3c0abaec889acbcb83
tianye1 <tianye1@360.cn>
2018-04-27 14:15:45 +0800

    组件化

--- src/components/MultiSelectPanel.js
+++ src/components/MultiSelectPanel.js
@@ -1,5 +1,6 @@
-import React     from 'react';
-import _         from 'lodash';
+import React from 'react';
+import _     from 'lodash';
+import cx    from 'classnames';
 
 require('styles/components/MultiSelectPanel.scss')
 
@@ -19,26 +20,40 @@ class MultiSelectPanel extends React.Component {
 
   render() {
     const {leftSelects, rightSelects} = this.state;
+    const {warpClass} = this.props;
 
     return (
-      <div className="multi-select-panel">
+      <div className={cx('multi-select-panel',{[warpClass]: warpClass})}>
         <div className="left-selects">
           {this.renderSelectList('leftSelect', Object.values(leftSelects))}
         </div>
         <div className="middle-controllers">
-          <button onClick={()=>this.addList()}>添加</button>
-          <button onClick={()=>this.removeList()}>移除</button>
-          <button onClick={()=>this.controlAll('add')}>添加全部</button>
-          <button onClick={()=>this.controlAll('remove')}>移除全部</button>
+          <button onClick={()=>this.addList()}>添加 </button>
+          <button onClick={()=>this.removeList()}> 移除</button>
+          <button onClick={()=>this.controlAll('add')}>添加全部 </button>
+          <button onClick={()=>this.controlAll('remove')}> 移除全部</button>
         </div>
         <div className="right-selects">
           {this.renderSelectList('rightSelect', Object.values(rightSelects))}
         </div>
         <div className="confirm-selects">
-          <button type="button">确定</button>
+          <button onClick={()=>this.returnClick('confirm')}>确定</button>
+          <button onClick={()=>this.returnClick('cancel')}>取消</button>
         </div>
       </div>
     );
+  }
+
+  returnClick(type) {
+    const {submitFun, cancelFun} = this.props;
+    const {leftSelects, rightSelects} = this.state;
+    
+    if (type === 'confirm' && submitFun && typeof submitFun === 'function') {
+      submitFun(leftSelects, rightSelects);
+    }
+    if (type === 'cancel' && cancelFun && typeof cancelFun === 'function') {
+      cancelFun(leftSelects, rightSelects);
+    }
   }
 
   initData() {
@@ -52,7 +67,7 @@ class MultiSelectPanel extends React.Component {
   setInitState(name,date) {
     if (!date) return;
     const newDate = {}
-    date.map((item) => {
+    date.forEach((item) => {
       if (item.id) {
         newDate[item.id] = item;
       }
@@ -92,7 +107,7 @@ class MultiSelectPanel extends React.Component {
     const newRight = Object.assign(rightSelects);
     if (readyleftSelect && readyleftSelect.length) {
       const leftKeys = Object.keys(leftSelects);
-      readyleftSelect.map((item)=>{
+      readyleftSelect.forEach((item)=>{
         if (leftKeys.indexOf(item)>=0) {
           newRight[item] = newLeft[item];
           _.unset(newLeft,item);
@@ -111,7 +126,7 @@ class MultiSelectPanel extends React.Component {
     const newRight = Object.assign(rightSelects);
     if (readyrightSelect && readyrightSelect.length) {
       const rightKeys = Object.keys(rightSelects);
-      readyrightSelect.map((item)=>{
+      readyrightSelect.forEach((item)=>{
         if (rightKeys.indexOf(item)>=0) {
           newLeft[item] = newRight[item];
           _.unset(newRight,item);
@@ -129,10 +144,10 @@ class MultiSelectPanel extends React.Component {
     
     let newLeft = {};
     let newRight = {};
-    if (type == 'add') {
+    if (type === 'add') {
       newRight = Object.assign(rightSelects,leftSelects);
     }
-    if (type == 'remove') {
+    if (type === 'remove') {
       newLeft = Object.assign(leftSelects,rightSelects);
     }
     this.setStateWith('leftSelects',newLeft);
@@ -151,24 +166,6 @@ class MultiSelectPanel extends React.Component {
       this.setState({[name]: value})
     }
   }
-}
-
-MultiSelectPanel.defaultProps = {
-  leftSelects: [
-    {id: '606923582', title: '哈哈哈测试上传配图绑定'},
-    {id: '606923178', title: '测试计划计划'}
-  ],
-  rightSelects: [
-    {id: '606922897', title: 'kkkk'},
-    {id: '606922825', title: '新的乱买词'},
-    {id: '606921622', title: '21新增的移动计划112312311'},
-    {id: '606921621', title: '测试测试...1'},
-    {id: '606921233', title: '客户端测试 计划hhh'},
-    {id: '606919079', title: '2222'},
-    {id: '606919078', title: '多线程计划'},
-    {id: '606918743', title: '1233456789'},
-    {id: '606918698', title: 'test_kune90909'}
-  ]
 }
 
 export default MultiSelectPanel;
